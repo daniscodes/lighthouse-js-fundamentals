71bf0aa1f0ff36c040447e11cd0ad30e71cb5807
daniscodes <dan97@bell.net>
2022-07-10 11:46:39 -0400

    just practicing

--- odd.js
+++ odd.js
@@ -4,3 +4,27 @@ const isOdd  = function (num) {
 
 console.log("3 is odd: " + isOdd(3));
 console.log("8 is odd: " + isOdd(8));
+
+const input = ['north', 'north', 'west', 'west', 'north', 'east','north'];  
+const output = [-1, 4];
+
+const finalPosition = (moves) => {
+  let x = 0;
+  let y = 0;
+
+  for(let i=0; i < moves.length; i++){
+    if(moves[i] === 'north') {
+      y++;
+    } else if (moves[i] === 'south') {
+      y--;
+    } else if (moves[i] === 'west') {
+      x--;
+    } else if (moves[i] === 'east') {
+      x++;
+    }
+  }
+  
+  return [x,y];
+};
+
+console.log(finalPosition(input));
